9d2c38ed90c5f1642e254f1df7d8108ebf4d10dd
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-15 11:58:42 -0700

    Finish test cases

--- test_solution.py
+++ test_solution.py
@@ -1,19 +1,22 @@
 from solution.py import square_growth_naive, square_growth
 
 def test_normal_case_1():
-        assert square_growth_naive() == [-2, 4, 16]
+        assert square_growth_naive([-2, -1, 4]) == [1, 4, 16]
 
 def test_normal_case_2():
-        assert square_growth_naive() == [3, 5, 9]
+        assert square_growth_naive([3, 5, 9]) == [9, 25, 81]
 
 def test_normal_case_3():
-        assert square_growth_naive() == [-1, 0, 1, 2, 3, 19]
+        assert square_growth_naive([-1, 0, 1, 2, 3, 19]) == [0, 1, 1, 4, 9, 361]
 
 def test_edge_case_1():
-        assert square_growth_naive() == [-8, -4, -3]
+#all negatives
+        assert square_growth_naive([-8, -4, -3]) == [9, 16, 64]
 
 def test_edge_case_2():
-        assert square_growth_naive() == []
+empty list
+        assert square_growth_naive([]) == []
 
 def test_edge_case_3():
-        assert square_growth_naive() == [0]
+#zero only
+        assert square_growth_naive([0]) == [0]
