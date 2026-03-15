4c8a2371282b0e9720047fc0a15a145dbc12fdc7
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-14 22:42:37 -0700

    New logical approach

--- solution.py
+++ solution.py
@@ -1,10 +1,30 @@
-def square_growth(growthPercentages):
-    for percentage in growthPercentages:
-        squared_percentage = percentage ** 2
-        squared_growth.append(squared_percentage)
+def square_growth_naive(growthPercentages):
+    squared_growth_array = []
+    squared_growth_stack = []
 
-    return squared_growth.sort()
+    while growthPercentages:
+        if abs(growthPercentages[0]) > abs(growthPercentages[-1]):
+            popped_percent = growthPercentages.pop(0)
+            squared_percent = popped_percent ** 2
+            squared_growth_stack.append(squared_percent) = 
+        else:
+            popped_percent = growthPercentages.pop()
+            squared_percent = popped_percent ** 2
+            squared_growth_stack.append(squared_percent)
+
+    squared_growth = squared_growth_stack.reverse()
+
+    return squared_growth
+
+# def square_growth(growthPercentages):
+#     squared_growth = []
+#     for percentage in growthPercentages:
+#         squared_percentage = percentage ** 2
+#         squared_growth.append(squared_percentage)
+
+#     squared_growth.sort()
+#     return squared_growth
 
 if __name__ == '__main__':
-    growthPercentages = [-3,-1,4,7]
+    growthPercentages = [-3, -1, 4, 7]
     print(square_growth(growthPercentages))
