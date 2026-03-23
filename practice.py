943db998d276056d389ae4a34ec0f1e2061131f9
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-22 21:27:37 -0700

    solution for reference during video

---
+++ practice.py
@@ -0,0 +1,53 @@
+def square_growth_naive(growthPercentages):
+    '''
+    Square elements from array and populate new array with squares sorted from least to greatest
+
+    Time complexity: O(n^2)
+    Space complexity: O(n)
+    '''
+    squared_growth_stack = []
+
+    while growthPercentages:
+        if abs(growthPercentages[0]) > abs(growthPercentages[-1]):
+            popped_percent = growthPercentages.pop(0)
+            squared_percent = popped_percent ** 2
+            squared_growth_stack.append(squared_percent)
+        else:
+            popped_percent = growthPercentages.pop()
+            squared_percent = popped_percent ** 2
+            squared_growth_stack.append(squared_percent)
+
+    squared_growth_stack.reverse()
+    squared_growth = squared_growth_stack
+
+    return squared_growth
+##################
+def square_growth(growthPercentages):
+    '''
+    Square elements from array and populate new array with squares sorted from least to greatest. Optimized from O(n^2) to O(n) by avoiding shifting caused by pop(0) and by avoiding a reverse.
+
+    Time complexity: O(n)
+    Space complexity: O(n)
+    '''
+
+    list_length = len(growthPercentages)
+    squared_growth = [0] * list_length
+    fill = list_length - 1
+    right = list_length - 1
+    left = 0
+    while right >= left:
+        if abs(growthPercentages[left]) > abs(growthPercentages[right]):
+            squared_percentage = growthPercentages[left] ** 2
+            squared_growth[fill] = squared_percentage
+            left += 1
+        else:
+            squared_percentage = growthPercentages[right] ** 2
+            squared_growth[fill] = squared_percentage
+            right -= 1
+        fill -= 1
+    return squared_growth
+
+if __name__ == '__main__':
+    growthPercentages = [-3, -1, 4, 7]
+    print(square_growth_naive(growthPercentages))
+    print(square_growth(growthPercentages))
